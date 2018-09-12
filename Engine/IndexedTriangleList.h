177a09cce4e29476c2babbf26ed89bbdb2d6d26f
kathelas <athelaswow@gmail.com>
2018-09-12 23:23:15 +0200

    added cullflags

--- Engine/IndexedTriangleList.h
+++ Engine/IndexedTriangleList.h
@@ -5,6 +5,16 @@
 
 struct IndexedTriangleList
 {
+	IndexedTriangleList( std::vector<Vec3> vert_in, std::vector<size_t> index_in )
+		:
+		vertices( std::move( vert_in ) ),
+		indices( std::move( index_in ) )
+	{
+		assert( vertices.size() > 2 );
+		assert( indices.size() % 3 == 0 );
+		cullFlags.resize( indices.size() / 3, false );
+	}
 	std::vector<Vec3> vertices;
 	std::vector<size_t> indices;
+	std::vector<bool> cullFlags;
 };
